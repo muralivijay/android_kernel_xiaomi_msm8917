3a1e3d93ef3be9ae2deaea673ebd60a0c5a3ec17
nichcream <c.potato23@gmail.com>
2017-08-12 15:20:36 +0800

    cpufreq: despair: include needed Per policy instances

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -193,6 +193,9 @@ struct cs_dbs_tuners {
 	unsigned int up_threshold;
 	unsigned int down_threshold;
 	unsigned int freq_step;
+	unsigned int twostep_threshold;
+	unsigned int twostep_counter;
+	u64 twostep_time;
 };
 
 struct ex_dbs_tuners {
